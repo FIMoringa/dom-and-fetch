ecf9b82928b75b9535799d07de1f1db632d9c2ec
Titus Muthomi <muthomititus+github@gmail.com>
2022-12-01 10:06:01 +0300

    Get students from the server and display their first name

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
+document.addEventListener('DOMContentLoaded', () => {
+	const studentDiv = document.querySelector('#students');
+	// studentDiv.innerHTML = 'Levy';
+	// stduentDiv.textContent = 'Levy';
 
+	const fetchStudents = () => {
+		fetch('http://localhost:3000/students')
+			.then((response) => response.json())
+			.then((students) =>
+				// students.forEach((item) => (studentDiv.innerHTML = item.firstName))
+				{
+					for (let student of students) {
+						stdudentName = document.createElement('p');
+
+						stdudentName.innerHTML = student.firstName;
+
+						studentDiv.append(stdudentName);
+					}
+				}
+			);
+	};
+
+	fetchStudents();
+});
