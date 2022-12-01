be0c4e47b5db2c31ad27d83cab06eb3791230bce
Titus Muthomi <muthomititus+github@gmail.com>
2022-12-01 10:17:27 +0300

    Add db link, use asynch await

--- index.js
+++ index.js
@@ -1,24 +1,44 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', async () => {
 	const studentDiv = document.querySelector('#students');
 	// studentDiv.innerHTML = 'Levy';
 	// stduentDiv.textContent = 'Levy';
 
-	const fetchStudents = () => {
-		fetch('http://localhost:3000/students')
-			.then((response) => response.json())
-			.then((students) =>
-				// students.forEach((item) => (studentDiv.innerHTML = item.firstName))
-				{
-					for (let student of students) {
-						stdudentName = document.createElement('p');
+	// const fetchStudents = () => {
+	// 	fetch('http://localhost:3000/students')
+	// 		.then((response) => response.json())
+	// 		.then((students) =>
+	// 			{
+	// 				for (let student of students) {
+	// 					stdudentName = document.createElement('p');
 
-						stdudentName.innerHTML = student.firstName;
+	// 					stdudentName.innerHTML = student.firstName;
 
-						studentDiv.append(stdudentName);
-					}
-				}
-			);
+	// 					studentDiv.append(stdudentName);
+	// 				}
+	// 			}
+	// 		);
+	// };
+
+	// fetchStudents();
+
+	const asyncGetStudents = async () => {
+		const res = await fetch('http://localhost:3000/students');
+		const students = await res.json();
+		return students;
 	};
 
-	fetchStudents();
+	const data = await asyncGetStudents();
+
+	data.forEach(student => {
+    studentParagraph = document.createElement('p')
+    studentParagraph.innerHTML = student.firstName
+    studentDiv.append(studentParagraph)
+  })
 });
+
+// syncronous
+
+// asyncronous
+
+// deb.json link
+// https://www.npmjs.com/package/json-server
